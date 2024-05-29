88e593aef4900117ba87e34213ec82b53952c428
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-29 11:40:36 +0100

    Versão 2

--- MultipleStuff/Program.cs
+++ MultipleStuff/Program.cs
@@ -16,5 +16,22 @@ namespace MultipleStuff
             min = this.Min();
             max = this.Max();
         }
+    
+
+        public class MinMax
+        {
+            public double Min { get; set; }
+            public double Max { get; set; }
+        }
+
+        public MinMax GetMinMax2()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty.");
+            }
+
+            return new MinMax { Min = this.Min(), Max = this.Max() };
+        }
     }
 }
