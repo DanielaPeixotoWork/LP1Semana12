7e9332ac2f956e891cee16539a51284efb3d305b
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-29 11:38:55 +0100

    Versão 1

---
+++ MultipleStuff/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MultipleStuff
+{
+    class SuperList : List<double>
+    {
+        public void GetMinMax1(out double min, out double max)
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty.");
+            }
+
+            min = this.Min();
+            max = this.Max();
+        }
+    }
+}
